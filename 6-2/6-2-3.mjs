cb1c90dba1edb32df5483599b5d81a9d615cff42
HC421013 <nkhshc421013@g.neec.ac.jp>
2024-12-23 13:03:54 +0900

    6-2-1

---
+++ 6-2/6-2-3.mjs
@@ -0,0 +1,7 @@
+const arrayNums = []
+
+arrayNums[0] = 100;
+arrayNums[1] = -56;
+
+console.log(arrayNums[0])
+console.log(arrayNums[1])
---
+++ 6-2/6-2-4.mjs
@@ -0,0 +1,5 @@
+const arrayTexts = []
+
+arrayTexts[0] = 'AddText';
+
+console.log(arrayTexts[0])
