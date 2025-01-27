fe5e74b6e81fc04409c77a0500c78039b992ac53
HC421013 <nkhshc421013@g.neec.ac.jp>
2025-01-27 12:17:40 +0900

    6-7-2 up

--- 6-7/6-7-2.mjs
+++ 6-7/6-7-2.mjs
@@ -1,6 +1,5 @@
-const values = ["A", "B", "A", "C", "A"]
-const valueSet = new Set(values)
-for (const value of valueSet) 
-{
-    console.log(value)
+const values = [1,2,3,3,3]
+const valueset = new Set(values)
+for (const str of valueset) {
+    console.log(str)
 }
