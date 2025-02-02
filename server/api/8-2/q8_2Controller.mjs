87ad160b72fd319daa9400973b9306e732a77c01
HC421013 <nkhshc421013@g.neec.ac.jp>
2025-02-02 23:14:46 +0900

    8-2-1

---
+++ server/api/8-2/q8_2Controller.mjs
@@ -0,0 +1,28 @@
+const get8_2_1 = (req, res) => 
+{
+    const { code } = req.query;
+
+    const predefinedData = {
+        code: "HC42-9821",
+        name: "情報太郎"
+    }
+
+    if (code === predefinedData.code) {
+        res.send(JSON.stringify({
+            code: predefinedData.code,
+            name: predefinedData.name
+        }))
+    } else {
+        res.send(JSON.stringify({
+            status: "error",
+            code: code,
+            cause: "not found code!"
+        }))
+    }
+
+}
+
+export const q8_2Controller = 
+{
+    get8_2_1,
+}
